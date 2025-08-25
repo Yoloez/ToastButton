a401c525fc2ba8e2819f8f983bafc9d4e6dc328d
praktikan <praktikan@SV-EIHU112-011.local>
2025-08-25 15:14:57 +0700

    first commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.hellotoast
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hellotoast.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var number: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //setting binding inflate
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // setting content dari layar activity
+        setContentView(binding.root)
+
+        with(binding){
+            txtNumber.text = number.toString()
+
+            btnCount.setOnClickListener{
+                number++
+                txtNumber.text = number.toString()
+            }
+
+            btnToast.setOnClickListener {
+                Toast.text(this@MainActivity, "Sekarang angka $number", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
